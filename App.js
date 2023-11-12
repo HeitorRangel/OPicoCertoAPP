18461d8e9d59ae74210e3476fa56e168fe3c20b9
Heitor Espinosa de C. Rangel <87882995+HeitorRangel@users.noreply.github.com>
2023-11-12 18:48:32 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { Routes } from './src/routes';
+import React, { useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+export default function App() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    // Após um certo tempo determinado, ou ao finalizar algum carregamento necessário
+    setTimeout(() => {
+      navigation.navigate('Home');
+    }, 3000); // Navegar para "Home" após 3 segundos (3000 milissegundos)
+  }, [navigation]);
+
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  );
+}
+
