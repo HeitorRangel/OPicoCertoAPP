f249ab579d87073c70aaac1c425384831b34e3ce
Heitor Espinosa de C. Rangel <87882995+HeitorRangel@users.noreply.github.com>
2023-11-16 10:19:39 -0300

    routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,43 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from './pages/home'; // Remova a referência a 'index.js'
+import Home from './pages/home';
 import CityDetails from './pages/CityDetails';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export function Routes() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator 
+      screenOptions={{
+        tabBarActiveBackgroundColor: '#1DAD9C',
+        tabBarActiveTintColor: '#fff',
+        tabBarInactiveTintColor: '#1B2727',
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: 'absolute',
+          backgroundColor: '#E5E5E5',
+          borderTopWidth: 0,
+          bottom: 14,
+          left: 14,
+          right: 14,
+          elevation: 0,
+          borderRadius: 8,
+          height: 60,
+        },
+      }}
+    >
       <Tab.Screen
-        name="home"
-        component={Home} // Use 'Home' com a primeira letra maiúscula
+        name="Home"
+        component={Home}
         options={{
           headerShown: false,
+          tabBarIcon: ({ color, size, focused }) => (
+            focused ? (
+              <Ionicons name="home" size={size} color={color} />
+            ) : (
+              <Ionicons name="home-outline" size={size} color={color} />
+            )
+          ),
         }}
       />
       <Tab.Screen
@@ -19,10 +45,15 @@ export function Routes() {
         component={CityDetails}
         options={{
           headerShown: false,
+          tabBarIcon: ({ color, size, focused }) => (
+            focused ? (
+              <Ionicons name="map" size={size} color={color} />
+            ) : (
+              <Ionicons name="map-outline" size={size} color={color} />
+            )
+          ),
         }}
       />
     </Tab.Navigator>
   );
 }
-
-export default Routes;
