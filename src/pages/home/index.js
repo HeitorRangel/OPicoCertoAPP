ccd8291343466f40afb2683656ec7a12cc9ea9de
Heitor Espinosa de C. Rangel <87882995+HeitorRangel@users.noreply.github.com>
2023-11-16 10:15:57 -0300

    index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, ScrollView, TouchableWithoutFeedback } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { styles } from '../../style';
 
 const citiesData = [
   { city: 'Rio de Janeiro', description: 'Praia de Copacabana' },
   { city: 'Florianópolis', description: 'Praia da Joaquina' },
+  { city: 'Macaé', description: 'Praia dos Cavaleiros' },
   // Mais cidades...
 ];
 
@@ -12,22 +14,38 @@ const Home = () => {
   const navigation = useNavigation();
 
   const goToCityDetails = (city) => {
-    // Navega para a tela de detalhes quando um item do carrossel é pressionado
     navigation.navigate('CityDetails', { city });
   };
 
   return (
-    <View style={{ flex: 1 }}>
-      <Text>Olá</Text>
-      <ScrollView horizontal>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Olá</Text>
+      </View>
+      <ScrollView horizontal style={styles.scrollView}>
         {citiesData.map((city, index) => (
-          <TouchableOpacity key={index} onPress={() => goToCityDetails(city)}>
-            {/* Renderização do carrossel */}
-          </TouchableOpacity>
+          <TouchableWithoutFeedback
+            key={index}
+            onPress={() => goToCityDetails(city)}
+          >
+            <View style={styles.carouselSlide}>
+              <Text style={styles.slideText}>
+                {city.city}
+                {"\n"}
+                {city.description}
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
         ))}
       </ScrollView>
+      <View style={styles.bottomMenu}>
+        <Text style={styles.bottomMenuText}>Menu Inferior</Text>
+      </View>
     </View>
   );
 };
 
 export default Home;
+
+
+
